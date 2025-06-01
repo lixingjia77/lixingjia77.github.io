ad00e46d1853eaa556fa98308488076b6440a2a1
lixingjia <xingjia.li@qq.com>
2025-06-01 21:39:57 +0800

    update

--- src/.vuepress/sidebar.ts
+++ src/.vuepress/sidebar.ts
@@ -17,10 +17,10 @@ export default sidebar({
       children: "structure",
     },
     "intro",
-    {
-      text: "幻灯片",
-      icon: "person-chalkboard",
-      link: "https://ecosystem.vuejs.press/zh/plugins/markdown/revealjs/demo.html",
-    },
+    // {
+    //   text: "幻灯片",
+    //   icon: "person-chalkboard",
+    //   link: "https://ecosystem.vuejs.press/zh/plugins/markdown/revealjs/demo.html",
+    // },
   ],
 });
